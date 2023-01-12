266764e0a0108fb6393d57739c9cfa623569a5ce
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2023-01-12 17:44:14 -0600

    Agregar archivos de proyecto.

---
+++ Pruebaas/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Programa 1");
+Console.WriteLine("Cual es tu nombre? ");
+String nombre = Console.ReadLine();
+Console.WriteLine("Tu nombre es " + nombre);
+Console.WriteLine("Programa 2");
+Console.WriteLine("La hora es " + DateTime.Now.ToShortTimeString());
+    
+
+Console.ReadKey();
